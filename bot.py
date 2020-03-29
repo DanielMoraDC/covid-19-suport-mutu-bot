cae45ca5d36df4a43ca04a5e7ddb9c824e3e0c30
Daniel <daniel.mora.checa@gmail.com>
2020-03-29 11:59:22 +0200

    Canviar llibreria a python-telegram-bot
    Funcionament més intuitiu que l'anterior i podria solucionar
    problemes de timeout existents.

--- bot.py
+++ bot.py
@@ -1,45 +1,65 @@
-import telebot
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
 
 import messages
 from locations import Location
 from groups import groups_by_distance
 
 TOKEN = '<telegram_token>'
-bot = telebot.TeleBot(TOKEN)
-
 CLOSEST_GROUPS = 3
 
 
-@bot.message_handler(commands=['info'])
-def welcome_message(message):
-    bot.register_next_step_handler(message, buscar_grup)
-    bot.reply_to(message, messages.WELCOME.encode('utf-8'))
+def welcome_callback(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text=messages.WELCOME)
 
 
-@bot.message_handler(func=lambda m: True)
-def buscar_grup(message):
-    if message.content_type == 'location':
-        location = Location(latitude=message.json['location']['latitude'],
-                            longitude=message.json['location']['longitude'])
+def find_group_callback(update, context):
+    message = update.message
+    location = Location(latitude=message.location.latitude,
+                        longitude=message.location.longitude)
 
-        groups = groups_by_distance(location)
-        closest = groups[:CLOSEST_GROUPS]
-        groups_display_str = '\n'.join(list(map(str, closest)))
+    groups = groups_by_distance(location)
+    closest = groups[:CLOSEST_GROUPS]
+    groups_display_str = '\n'.join(list(map(str, closest)))
 
-        if closest[0].distance > 1.0:
-            response = messages.LOCATIONS_CLOSE_TEMPLATE_NOK.format(
-                group_info=groups_display_str
-            )
-        else:
-            response = messages.LOCATIONS_CLOSE_TEMPLATE_OK.format(
-                group_info=groups_display_str
-            )
-
+    if closest[0].distance > 1.0:
+        response = messages.LOCATIONS_CLOSE_TEMPLATE_NOK.format(
+            group_info=groups_display_str
+        )
     else:
-        response = messages.NOT_LOCATION
+        response = messages.LOCATIONS_CLOSE_TEMPLATE_OK.format(
+            group_info=groups_display_str
+        )
 
-    bot.reply_to(message, response.encode('utf-8'))
-    bot.register_next_step_handler(message, buscar_grup)
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text=response)
 
 
-bot.polling(none_stop=True)
+def not_location_callback(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text=messages.NOT_LOCATION)
+
+
+# Set token and dispatcher
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+# Define bot start handler
+start_handler = CommandHandler('start', welcome_callback)
+dispatcher.add_handler(start_handler)
+
+# On location, retrieve closest group
+location_handler = MessageHandler(Filters.location, find_group_callback)
+dispatcher.add_handler(location_handler)
+
+# If location not provided, send warning
+non_location_handler = MessageHandler(~Filters.location, not_location_callback)
+dispatcher.add_handler(non_location_handler)
+
+
+if __name__ == '__main__':
+    updater.start_polling()
